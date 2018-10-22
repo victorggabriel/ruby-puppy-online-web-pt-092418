377b89a959c2dcff67f796f80577559484dc9a4a
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-22 15:52:35 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+      @@all.clear
+  end
+
+  def self.all
+    @@all.each do |x|
+      puts x.name
+    end
+  end
+
+end
